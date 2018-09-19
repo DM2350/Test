a3375e44b5e4ad4f219ca75b48256ee03d697958
Per <Paeron@users.noreply.github.com>
2018-09-19 14:27:33 +0200

    Some ideas on for main.js
    May not work at all

--- main.js
+++ main.js
@@ -8,3 +8,62 @@
 // 		Question med next press X eller liknande
 //		Spara info
 //		Slutmeny
+
+colourList = [];
+globalListOfData = [];
+
+function viewChanger(viewString) {
+
+	if(viewString == "start") {
+		// Block thing show things
+	} else if(viewString == "text") {
+
+	} else if(viewString == "noise") {
+
+	} else if(viewString == "question") {
+
+	} else if(viewString == "saving") {
+
+	} else if(viewString == "end") {
+
+	} else {
+		console.log("ERROR! viewChanger did not recieve proper string");
+	}
+    
+}
+
+function readLists() {
+	// TODO fyller i vettig info i colourList och globalListOfData
+}
+
+function start() {
+	viewChanger("start")
+}
+
+window.addEventListener("keydown", keyDownFunction);
+
+function keyDownFunction() {
+    // TODO KOLLA OM DET ÄR RÄTT/FEL SVAR
+    // SPARA DATA i listan
+    //
+   // if right or wring
+   //
+    listLooper()
+}
+
+function listLooper(){
+	window.getElementById("text").innerHTML = getRandomText()
+	// TODO FIX ANSWER FOR TEXT
+	window.getElementById("answer").innerHTML = "XXXX"
+    viewChanger("text")
+    setTimeout(viewString("noise"), 500)
+    setTimeout(viewChanger("question"), 1000)
+}
+
+function getRandomText() {
+	// TODO FIX THIS
+	return "TODO"
+}
+
+
+
