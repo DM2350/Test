419d8ab34e412840dc4da01928c7bf6c85de6bc4
Paeron <perjaa@kth.se>
2018-09-21 15:12:21 +0200

    Todo list

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
 // TODO:
-// LÄSA IN LISTOR
-// TIMING GREJ
-// SKAPA VIEWS
-//		Start
-//		Text
-//		Brus
-// 		Question med next press X eller liknande
-//		Spara info
-//		Slutmeny
+// Läsa in listor
+// Justera timing för stimuli
+// Fixa random text
+// Spara ner info
+// Kolla vilken tangentbordsknapp som tryckts ner
+// Add timing för svar
+// Färg
+// Design
 
-// ADD TIMING FOR ANSWERS
-
+// Global Variables
 colourList = [];
 globalListOfData = [];
 
+// All colours
+green	= '#247e18'
+red 	= '#6D1515'
+
+blueBlue 	= '#15156d'
+blueGreen 	= '#156d5d'
+blueViolet	= '#53156d'
+greenGreen	= '#1f6d15'
+orangeOrange= '#df8c16'
+redRed		= '#7f1717'
+redOrange	= '#b24217'
+redViolet	= '#6d1549'
+violetViolet= '#68156d'
+yellowYellow= '#d0cc2a'
+yellowGreen = '#71941c'
+yellowOrange= '#d0aa2a'
+
 function viewChanger(viewString) {
 
-	if(viewString == "start") {
+	if(viewString == "start") { 
+		document.getElementById("image").style.display = "none"
 		document.getElementById("thanks").style.display = "none"
 		document.getElementById("testText").style.display = "none"
 		document.getElementById("answer").style.display = "none"
 		document.getElementById("sameAsYouSaw").style.display = "none"
 		document.getElementById("startButton").style.display = "block"
 	} else if(viewString == "text") {
+		document.getElementById("image").style.display = "none"
 		document.getElementById("sameAsYouSaw").style.display = "none"
 		document.getElementById("answer").style.display = "none"
 		document.getElementById("testText").style.display = "block"
@@ -30,9 +47,11 @@ function viewChanger(viewString) {
 	} else if(viewString == "noise") {
 		document.getElementById("testText").style.display = "none"
 	} else if(viewString == "question") {
+		document.getElementById("image").style.display = "block"
 		document.getElementById("sameAsYouSaw").style.display = "block"
 		document.getElementById("answer").style.display = "block"
 	} else if(viewString == "end") {
+		document.getElementById("image").style.display = "none"
 		document.getElementById("sameAsYouSaw").style.display = "none"
 		document.getElementById("answer").style.display = "none"
 		document.getElementById("thanks").style.display = "block"
@@ -46,27 +65,38 @@ function readLists() {
 }
 
 function start() {
-	viewChanger("start")
+	//viewChanger("text")
+	listLooper()
 }
 
 window.addEventListener("keydown", keyDownFunction);
 
 function keyDownFunction() {
-    // TODO KOLLA OM DET ÄR RÄTT/FEL SVAR
-    // SPARA DATA i listan
-    //
+
+   // TODO KOLLA OM DET ÄR RÄTT/FEL SVAR
+   // SPARA DATA i listan
+   //
    // if right or wring
    //
-    listLooper()
+   listLooper()
 }
 
 function listLooper(){
-	window.getElementById("testText").innerHTML = getRandomText()
+	viewChanger("text")
+	document.getElementById("testText").innerHTML = getRandomText()
 	// TODO FIX ANSWER FOR TEXT
-	window.getElementById("sameAsYouSaw").innerHTML = "XXXX"
+	document.getElementById("answer").innerHTML = "XXXX"
     viewChanger("text")
-    setTimeout(viewString("noise"), 500)
-    setTimeout(viewChanger("question"), 1000)
+    setTimeout(noise, 1000)
+    setTimeout(question, 2000)
+}
+
+function question() {
+	viewChanger("question")
+}
+
+function noise() {
+	viewChanger("noise")
 }
 
 function getRandomText() {
@@ -74,5 +104,5 @@ function getRandomText() {
 	return "TODO"
 }
 
-
+viewChanger("start")
 
