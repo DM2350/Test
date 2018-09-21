e026fb1f3313f4d18c12cab0b849461ab461bf82
Paeron <perjaa@kth.se>
2018-09-21 15:40:25 +0200

    list fix
    listMaker skapar nu en sträng lik orginalet som vi kan ha att matcha med
    när strängen ska se annorlunda ut.

--- listMaker.py
+++ listMaker.py
@@ -8,19 +8,28 @@ strings = 4 #CHANGE WHEN NECESSARY
 capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 capLength = len(capitalLetters)
 
-stringList = []
+longList = []
 
 for i in range(0,100):
+	tempList = list()
 	tempString = str()
 	for j in range(0,strings):
 		tempString += capitalLetters[randint(0, capLength-1)]
-	stringList.append(tempString)
+	tempList.append(tempString)
 
-print(stringList)
+	tempString2 = tempString
+	while(tempString == tempString2):
+		changer = randint(0, strings-1)
+		listA = list(tempString2)
+		listA[changer] = capitalLetters[randint(0, capLength-1)]
+		tempString2 = ''.join(listA)
+	tempList.append(tempString2)
+	longList.append(tempList)
+
 
 with open('list_strings_' + str(strings) +  '.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for item in stringList:
+    for item in longList:
     	print(item)
     	spamwriter.writerow(item)
